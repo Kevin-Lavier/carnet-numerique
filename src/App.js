f7a333725055b4545a7ad940a941883b1e94a99c
Kevin Lavier <kevinlavier@orange.fr>
2024-11-06 14:01:06 +0100

    add : connect firebase, hosting on https://carnet-numerique-b5d04.web.app/

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bienvenue sur Carnet Numérique</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ceci est une application PWA de test pour vérifier si elle fonctionne
+          sur ton iPhone.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
